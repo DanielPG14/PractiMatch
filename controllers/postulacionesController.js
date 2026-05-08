6e79f6f5d7215070455fd91ce1148be2ea131c81
diazjazmin10x-afk <diazjazmin10x@gmail.com>
2026-05-08 00:35:48 -0600

    Refactor obtenerPostulaciones to use SQL query

--- controllers/postulacionesController.js
+++ controllers/postulacionesController.js
@@ -1,14 +1,49 @@
-const { listarTodos } = require('../models/model_consultas');
+const db = require('../config/database');
 
-const obtenerPostulaciones = async (req, res) => {
-    try {
-        // Usamos tu función dinámica 'listarTodos'
-        const datos = await listarTodos('postulaciones');
-        res.json(datos);
-    } catch (error) {
-        console.error("Error en el controlador:", error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
+exports.getAll = (req, res) => {
+
+    const query = `
+    
+        SELECT
+        
+            postulaciones.id_postulacion,
+            postulaciones.estatus,
+
+            estudiantes.id_estudiante,
+            estudiantes.matricula,
+
+            usuarios.correo,
+
+            vacantes.tipo_proceso
+
+        FROM postulaciones
+
+        INNER JOIN estudiantes
+            ON postulaciones.id_estudiante = estudiantes.id_estudiante
+
+        INNER JOIN usuarios
+            ON estudiantes.id_usuario = usuarios.id_usuario
+
+        INNER JOIN vacantes
+            ON postulaciones.id_vacante = vacantes.id_vacante
+    `;
+
+    db.query(query, (err, results) => {
+
+        if (err) {
+
+            return res.status(500).json({
+                success: false,
+                error: err
+            });
+
+        }
+
+        res.json({
+            success: true,
+            data: results
+        });
+
+    });
+
 };
-
-module.exports = { obtenerPostulaciones };
